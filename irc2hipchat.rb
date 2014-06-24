e5bdaac34b2f533c0b8ae63d8e7b8a31316c9d38
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-06-24 20:26:48 +0200

    Daemon IRC 2 HipChat logger

---
+++ irc2hipchat.rb
@@ -0,0 +1,25 @@
+require 'cinch'
+require 'hipchat'
+
+unless ENV.has_key?('HIPCHAT_API_TOKEN')
+  abort 'Set HIPCHAT_API_TOKEN environment variable with your HipChat API Token: https://hipchat.com/account/api'
+end
+
+$hc_room = 'AppNetaIRCLog'
+$hipchat_cli = HipChat::Client.new(ENV['HIPCHAT_API_TOKEN'], :api_version => 'v2')
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick     = 'loggerbot'
+    c.server   = 'irc.freenode.org'
+    c.channels = ["#appneta"]
+  end
+
+  # Only log channel messages
+  on :channel do |m|
+    $hipchat_cli[$hc_room].send(m.user.nick, m.message)
+  end
+end
+
+bot.start
+
