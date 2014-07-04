87454135b3684092ad6f45c2e98c56f0acef1636
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-07-05 00:58:28 +0200

    Hard code a fully qualified ruby path

--- lib/windows.rb
+++ lib/windows.rb
@@ -1,6 +1,10 @@
 require "win32/service"
 include Win32
 
+# We have no good way to detect where Ruby is installed unfortunately
+# Update this line with the fully qualified path to your ruby executable
+RUBY_EXECUTABLE="C:\\Ruby200\\bin\\ruby"
+
 class IRC2HipChat
   class Windows
     class << self
@@ -8,6 +12,10 @@ class IRC2HipChat
        # Register IRC2Hipchat as a Windows Service
        #
       def register_service
+        unless File.exists?(RUBY_EXECUTABLE)
+          abort("Sorry: We can't find your Ruby executable.  Please update" +
+                "lib/windows.rb line 6 with the full path to your Ruby executable.")
+        end
 
         Service.create({
           :service_name => 'IRC2HipChat',
@@ -16,7 +24,9 @@ class IRC2HipChat
           :description => 'Log all messages from an IRC channel to a HipChat room',
           :start_type => Service::AUTO_START,
           :error_control => Service::ERROR_NORMAL,
-          :binary_path_name => "ruby -C #{`echo %cd%`.chomp} irc2hipchat.rb",
+          # Windows Services need a fully qualified path to the ruby binary.
+          # Update this path and delete/re-add the service if it's broken for you
+          :binary_path_name => "#{RUBY_EXECUTABLE} -C #{`echo %cd%`.chomp} irc2hipchat.rb",
           :load_order_group => 'Network',
           :dependencies => nil,
           :display_name => 'IRC2HipChat'
